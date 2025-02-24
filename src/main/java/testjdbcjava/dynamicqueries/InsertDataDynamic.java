c004d232a646e215d97e0fee39b54fb1da7a3210
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-24 07:47:53 +0530

    inserting data dynamically using prepared statement and the help of delimiter

---
+++ src/main/java/testjdbcjava/dynamicqueries/InsertDataDynamic.java
@@ -0,0 +1,44 @@
+package testjdbcjava.dynamicqueries;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertDataDynamic {
+	private static String url = "jdbc:postgresql://localhost:5432/school";
+	private static String user = "postgres";
+	private static String password = "123";
+	
+	//main method
+	public static void main(String[] args) {
+		
+		try {
+			//1st step: Load & Register Driver Software
+			Class.forName("org.postgresql.Driver");
+			
+			//2nd step: Establishing connection
+			Connection conn = DriverManager.getConnection(url, user, password);
+			
+			//String query
+			String query = "INSERT into student values(?,?,?)";
+			
+			//3rd step: create Statement(PreparedStatement)
+			PreparedStatement pstm = conn.prepareStatement(query);
+			pstm.setInt(1, 100);
+			pstm.setString(2, "Raveesh");
+			pstm.setInt(3, 27);
+			
+			//4th step: Execute Query
+			pstm.execute();
+			System.out.println("Data Saved to database");
+			
+			//5th step: close the connection
+			conn.close();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
