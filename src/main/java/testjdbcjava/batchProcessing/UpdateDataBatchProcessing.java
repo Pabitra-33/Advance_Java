c6efab8ff36a7c38e23e2e141394c9af1818dd45
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-26 20:38:59 +0530

    updating the data using the batch processing where we can write multiple queries

---
+++ src/main/java/testjdbcjava/batchProcessing/UpdateDataBatchProcessing.java
@@ -0,0 +1,56 @@
+package testjdbcjava.batchProcessing;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class UpdateDataBatchProcessing {
+	private static String url = "jdbc:postgresql://localhost:5432/school";
+	private static String user = "postgres";
+	private static String password = "123";
+	static Connection conn;//Connection reference
+	
+	//main method
+	public static void main(String[] args) throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the teacher id: ");
+		int tid = sc.nextInt();
+		System.out.println("Enter the teacher name: ");
+		String tname = sc.next();
+		sc.nextLine();
+		
+		try {
+			//1st step: Load & Register Driver Software
+			Class.forName("org.postgresql.Driver");
+			
+			//2nd step: Establishing connection
+			conn = DriverManager.getConnection(url, user, password);
+			
+			//String query
+			String uQuery = "Update teacher set name = ? where id = ?";
+			String dQuery = "DELETE from teacher where id = ?";
+			
+			//3rd step: create Statement(PreparedStatement)
+			Statement stm = conn.createStatement();
+			stm.addBatch(dQuery);
+			stm.executeBatch();
+			
+			//4th step: Execute Query
+			stm.execute(uQuery);
+			stm.execute(dQuery);
+			System.out.println("Successfully updated data to database..!");
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			sc.close();
+			//5th step: close the connection
+			conn.close();
+		}
+	}
+}
