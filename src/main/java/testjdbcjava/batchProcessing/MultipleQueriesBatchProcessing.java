2cda1d4dae71f908b53e0c8d48e8bf07ba1603e9
Pabitra-33 <kumarpabitra345@gmail.com>
2025-02-27 18:20:21 +0530

    initial commit

--- src/main/java/testjdbcjava/batchProcessing/MultipleQueriesBatchProcessing.java
+++ src/main/java/testjdbcjava/batchProcessing/MultipleQueriesBatchProcessing.java
@@ -4,9 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
-public class UpdateDataBatchProcessing {
+public class MultipleQueriesBatchProcessing {
 	private static String url = "jdbc:postgresql://localhost:5432/school";
 	private static String user = "postgres";
 	private static String password = "123";
@@ -14,13 +13,7 @@ public class UpdateDataBatchProcessing {
 	
 	//main method
 	public static void main(String[] args) throws SQLException {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the teacher id: ");
-		int tid = sc.nextInt();
-		System.out.println("Enter the teacher name: ");
-		String tname = sc.next();
-		sc.nextLine();
-		
+	
 		try {
 			//1st step: Load & Register Driver Software
 			Class.forName("org.postgresql.Driver");
@@ -29,28 +22,26 @@ public class UpdateDataBatchProcessing {
 			conn = DriverManager.getConnection(url, user, password);
 			
 			//String query
-			String uQuery = "Update teacher set name = ? where id = ?";
-			String dQuery = "DELETE from teacher where id = ?";
+			String uQuery = "Update teacher set name = Ankita where id = 103";
+			String dQuery = "DELETE from teacher where id = 106";
 			
 			//3rd step: create Statement(PreparedStatement)
 			Statement stm = conn.createStatement();
+			stm.addBatch(uQuery);
 			stm.addBatch(dQuery);
 			stm.executeBatch();
 			
 			//4th step: Execute Query
-			stm.execute(uQuery);
-			stm.execute(dQuery);
-			System.out.println("Successfully updated data to database..!");
+			stm.executeUpdate(dQuery);
+			System.out.println("Successfully data updated in database..!");
 			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 		finally {
-			sc.close();
 			//5th step: close the connection
 			conn.close();
+			System.out.println("Connection Closed");
 		}
 	}
 }
