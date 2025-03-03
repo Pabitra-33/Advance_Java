0fe943c60c985d3abe5edcf63196edd3db52e773
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-03 20:43:18 +0530

    added the jdbc driver url and database credentials

--- src/main/java/testjdbcjava/batchProcessing/UpdateDataTestBatchProcessing.java
+++ src/main/java/testjdbcjava/batchProcessing/UpdateDataTestBatchProcessing.java
@@ -1,5 +1,10 @@
 package testjdbcjava.batchProcessing;
 
-public class UpdateDataTestBatchProcessing {
+import java.sql.Connection;
 
+public class UpdateDataTestBatchProcessing {
+	private static String url = "jdbc:postgresql://localhost:5432/school";
+	private static String user = "postgres";
+	private static String password = "123";
+	static Connection conn;//Connection reference
 }
