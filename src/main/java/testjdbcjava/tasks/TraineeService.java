811b091f6410f7a05071adb19e6cff2c4c2a97dd
Pabitra-33 <kumarpabitra345@gmail.com>
2025-03-08 17:14:45 +0530

    performs two steps to connect database in the static block

--- src/main/java/testjdbcjava/tasks/TraineeService.java
+++ src/main/java/testjdbcjava/tasks/TraineeService.java
@@ -1,17 +1,21 @@
 package testjdbcjava.tasks;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class TraineeService {
 	private static String url = "jdbc:postgresql://localhost:5432/jspider?user=postgres&password=123";
-	Connection con;
+	static Connection con;
 	
 	//static block
 	static {
 		try {
 			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection(url);
 		} catch (ClassNotFoundException e) {
-			
+			e.printStackTrace();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
